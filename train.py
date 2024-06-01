f3964d2280c2435fff8078e04472fda3452040f4
Trần Nguyễn Tiến Lộc <48644569+Junvildo@users.noreply.github.com>
2024-06-01 12:15:13 +0700

    Update train.py

--- train.py
+++ train.py
@@ -40,7 +40,7 @@ def train_d(net, data, label="real"):
     """Train function of discriminator"""
     if label=="real":
         part = random.randint(0, 3)
-        pred, [rec_all, rec_small, rec_part], part = net(data, label, part=part)
+        pred, [rec_all, rec_small, rec_part], part = net(data, label)
         err = F.relu(  torch.rand_like(pred) * 0.2 + 0.8 -  pred).mean() + \
             percept( rec_all, F.interpolate(data, rec_all.shape[2]) ).sum() +\
             percept( rec_small, F.interpolate(data, rec_small.shape[2]) ).sum() +\
