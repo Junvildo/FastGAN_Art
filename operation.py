5a9f8fed9708a7f9f4f75f96cc6c30261a094c8d
Trần Nguyễn Tiến Lộc <48644569+Junvildo@users.noreply.github.com>
2024-06-01 11:06:31 +0700

    Update operation.py

--- operation.py
+++ operation.py
@@ -10,6 +10,9 @@ import json
 
 def InfiniteSampler(n):
     """Data sampler"""
+    # check if the number of samples is valid
+    if n <= 0:
+        raise ValueError(f"Invalid number of samples: {n}.\nMake sure that images are present in the given path.")
     i = n - 1
     order = np.random.permutation(n)
     while True:
@@ -44,21 +47,27 @@ def load_params(model, new_param):
 
 
 def get_dir(args):
-    task_name = 'train_results/' + args.name
-    saved_model_folder = os.path.join( task_name, 'models')
-    saved_image_folder = os.path.join( task_name, 'images')
+
+    if not os.path.exists(args.output_path):
+        os.makedirs(args.output_path)
+        
+    task_name = os.path.join(args.output_path, 'train_results', args.name)
+    saved_model_folder = os.path.join(task_name, 'models')
+    saved_image_folder = os.path.join(task_name, 'images')
     
     os.makedirs(saved_model_folder, exist_ok=True)
     os.makedirs(saved_image_folder, exist_ok=True)
 
     for f in os.listdir('./'):
         if '.py' in f:
-            shutil.copy(f, task_name+'/'+f)
+            shutil.copy(f, os.path.join(task_name, f))
     
-    with open( os.path.join(saved_model_folder, '../args.txt'), 'w') as f:
+    with open(os.path.join(saved_model_folder, '../args.txt'), 'w') as f:
         json.dump(args.__dict__, f, indent=2)
 
     return saved_model_folder, saved_image_folder
+
+
 
 
 class  ImageFolder(Dataset):
@@ -95,137 +104,42 @@ class  ImageFolder(Dataset):
 
 
 from io import BytesIO
-import lmdb
-from torch.utils.data import Dataset
+# import lmdb
+# from torch.utils.data import Dataset
 
 
-class MultiResolutionDataset(Dataset):
-    def __init__(self, path, transform, resolution=256):
-        self.env = lmdb.open(
-            path,
-            max_readers=32,
-            readonly=True,
-            lock=False,
-            readahead=False,
-            meminit=False,
-        )
+# class MultiResolutionDataset(Dataset):
+#     def __init__(self, path, transform, resolution=256):
+#         self.env = lmdb.open(
+#             path,
+#             max_readers=32,
+#             readonly=True,
+#             lock=False,
+#             readahead=False,
+#             meminit=False,
+#         )
 
-        if not self.env:
-            raise IOError('Cannot open lmdb dataset', path)
+#         if not self.env:
+#             raise IOError('Cannot open lmdb dataset', path)
 
-        with self.env.begin(write=False) as txn:
-            self.length = int(txn.get('length'.encode('utf-8')).decode('utf-8'))
+#         with self.env.begin(write=False) as txn:
+#             self.length = int(txn.get('length'.encode('utf-8')).decode('utf-8'))
 
-        self.resolution = resolution
-        self.transform = transform
+#         self.resolution = resolution
+#         self.transform = transform
 
-    def __len__(self):
-        return self.length
+#     def __len__(self):
+#         return self.length
 
-    def __getitem__(self, index):
-        with self.env.begin(write=False) as txn:
-            key = f'{self.resolution}-{str(index).zfill(5)}'.encode('utf-8')
-            img_bytes = txn.get(key)
-            #key_asp = f'aspect_ratio-{str(index).zfill(5)}'.encode('utf-8')
-            #aspect_ratio = float(txn.get(key_asp).decode())
+#     def __getitem__(self, index):
+#         with self.env.begin(write=False) as txn:
+#             key = f'{self.resolution}-{str(index).zfill(5)}'.encode('utf-8')
+#             img_bytes = txn.get(key)
+#             #key_asp = f'aspect_ratio-{str(index).zfill(5)}'.encode('utf-8')
+#             #aspect_ratio = float(txn.get(key_asp).decode())
 
-        buffer = BytesIO(img_bytes)
-        img = Image.open(buffer)
-        img = self.transform(img)
+#         buffer = BytesIO(img_bytes)
+#         img = Image.open(buffer)
+#         img = self.transform(img)
 
-        return img
-
-
-from queue import Queue
-from threading import Thread
-class CudaDataLoader:
-    """ 异步预先将数据从CPU加载到GPU中 """
-
-    def __init__(self, loader, device, queue_size=2):
-        self.device = device
-        self.queue_size = queue_size
-        self.loader = loader
-
-        self.load_stream = torch.cuda.Stream(device=device)
-        self.queue = Queue(maxsize=self.queue_size)
-
-        self.idx = 0
-        self.worker = Thread(target=self.load_loop)
-        self.worker.setDaemon(True)
-        self.worker.start()
-
-    def load_loop(self):
-        """ 不断的将cuda数据加载到队列里 """
-        # The loop that will load into the queue in the background
-        while True:
-            for i, sample in enumerate(self.loader):
-                self.queue.put(self.load_instance(sample))
-
-    def load_instance(self, sample):
-        """ 将batch数据从CPU加载到GPU中 """
-        if torch.is_tensor(sample):
-            with torch.cuda.stream(self.load_stream):
-                return sample.to(self.device, non_blocking=True)
-        elif sample is None or type(sample) in (list, str):
-            return sample
-        elif isinstance(sample, dict):
-            return {k: self.load_instance(v) for k, v in sample.items()}
-        else:
-            return [self.load_instance(s) for s in sample]
-
-    def __iter__(self):
-        self.idx = 0
-        return self
-
-    def __next__(self):
-        # 加载线程挂了
-        if not self.worker.is_alive() and self.queue.empty():
-            self.idx = 0
-            self.queue.join()
-            self.worker.join()
-            raise StopIteration
-        # 一个epoch加载完了
-        elif self.idx >= len(self.loader):
-            self.idx = 0
-            raise StopIteration
-        # 下一个batch
-        else:
-            out = self.queue.get()
-            self.queue.task_done()
-            self.idx += 1
-        return out
-
-    def __len__(self):
-        return len(self.loader)
-
-    @property
-    def sampler(self):
-        return self.loader.sampler
-
-    @property
-    def dataset(self):
-        return self.loader.dataset
-
-class _RepeatSampler(object):
-    """ 一直repeat的sampler """
-
-    def __init__(self, sampler):
-        self.sampler = sampler
-
-    def __iter__(self):
-        while True:
-            yield from iter(self.sampler)
-
-class MultiEpochsDataLoader(torch.utils.data.DataLoader):
-    """ 多epoch训练时，DataLoader对象不用重新建立线程和batch_sampler对象，以节约每个epoch的初始化时间 """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        object.__setattr__(self, 'batch_sampler', _RepeatSampler(self.batch_sampler))
-        self.iterator = super().__iter__()
-
-    def __len__(self):
-        return len(self.batch_sampler.sampler)
-
-    def __iter__(self):
-        for i in range(len(self)):
-            yield next(self.iterator)
+#         return img
